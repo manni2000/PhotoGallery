0cbae86a63a9e6273c7090ff90263d13e9956f8b
Manish kumar <91480902+manni2000@users.noreply.github.com>
2023-03-31 23:58:37 +0530

    js update

--- index.js
+++ index.js
@@ -5,11 +5,12 @@ const search = document.getElementById("search");
 
 
 const key = "zPneqE9Acuj81SBzY32s6gxKcDlLJS1wIu2bScELw8PZdIpASvSyk0Jj"
-let url = `https://api.pexels.com/v1/curated`;
-let index = 1;
+const base_url = `https://api.pexels.com/v1/curated?page=1&per_page=15`;
+let url = base_url;
 
-const getMoreImages = (page_no) => {
-    fetch(url + `?page=${page_no}&per_page=15`, {
+const getMoreImages = () => {
+    console.log(url)
+    fetch(url, {
         method: "GET",
         withCredentials: true,
         headers: {
@@ -20,7 +21,7 @@ const getMoreImages = (page_no) => {
         .then(resp => resp.json())
         .then(function (data) {
             console.log(data)
-
+            url = data.next_page
             data.photos.forEach(element => {
                 const div = document.createElement('div')
                 div.classList.add('image-block')
@@ -33,17 +34,14 @@ const getMoreImages = (page_no) => {
         });
 }
 
-button.addEventListener('click', (e) => {
-    index += 1;
-    getMoreImages(index);
-})
+button.addEventListener('click', getMoreImages)
 
 form.addEventListener('submit', e => {
-    if(search.value === '') return;
-    index = 1
+    e.preventDefault()
     imageGrid.innerHTML = '';
-    url = `https://api.pexels.com/v1/search?query=${search.value}`;
-    getMoreImages(index, `search?query=${search.value}`)
+    if(search.value === '') url = base_url;
+    else url = `https://api.pexels.com/v1/search?query=${search.value}`;
+    getMoreImages()
 })
 
 getMoreImages(1);
